64f5f6acba83d3bd18aa70773c9ec603e75d2703
Pixyde <33604998+Pixyde@users.noreply.github.com>
2023-04-09 17:28:02 +0000

    Added error notification

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permission
 const fs = require('fs');
 const keepAlive = require('./server');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessageReactions] });
+const Errorhandler = require("discord-error-handler");
+const handle  = new Errorhandler(client, {
+  webhook: {id: process.env['webhook_id'], token: process.env['webhook_token']}
+})
+
+
+process.on('unhandledRejection', error => { 
+  handle.createrr(client, undefined, undefined, error)
+});
 
 client.commands = new Collection();
 
